3f22d326f54437c27b8acf353bb88d92f02cb8be
phamquangviet891 <20522153@gm.uit.edu.vn>
2023-09-24 20:20:55 +0700

    config vercel

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
+const morgan = require("morgan");
+//Logging in console
+app.use(morgan("tiny"));
 
+//env variables
+require("dotenv").config();
+//CORS
+var cors = require("cors");
+app.use(cors());
 app.get('/',(req,res)=> res.send("Hello from viet"))
-var server = app.listen(5000, console.log("server listening on port 5000"))
+const port = process.env.PORT || 5001
+var server = app.listen(port,()=>{
+    var port = server.address().port;
+    console.log("server listening on port ",port);
+})
