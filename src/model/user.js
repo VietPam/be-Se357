ce431da877e452c5fa6e5c4baa7613ff2d2fb846
phamquangviet891 <20522153@gm.uit.edu.vn>
2023-09-26 00:40:47 +0700

    add user model

---
+++ src/model/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    nameAccount:{
+        type: String,
+        default: '',
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    phone:{
+        type:String,
+        default:''
+    },
+    role:{
+        type: String,
+        required: true,
+        enum:['ADMIN','CUSTOMER'],
+        default:'CUSTOMER',
+    },
+    cart:{
+        type: [String],//productId
+        default:[]
+    },
+    createAt:{
+        type:Date,
+        required:true,
+        default:Date.now
+    }
+})
