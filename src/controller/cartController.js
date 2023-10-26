3f5162c91d0d5add9a05ae23a7992746a4e74010
phamquangviet891 <20522153@gm.uit.edu.vn>
2023-10-26 15:07:27 +0700

    post /cart/createCart

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -1,8 +1,8 @@
 const cartSchema = require('../model/cart')
 
-class ProductController{
+class CartController{
     async getCartByUserId(req,res){
-        const {userId}=req.params.id
+        const {userId}=req.params.userId
         try{
             const cart = await cartSchema.find({userId:userId})
             res.json(cart)
@@ -10,7 +10,23 @@ class ProductController{
             throw new Error(err);
         }
     }
+    async createCart(req,res,next){
+        //check for existing cart by userId
+        //if cart exist, return cart
+        //else create new cart
+        const findCart= cartSchema.find({userId:req.body.userId})
+        if(findCart){
+            return res.status(400).json({success:false,message:"Cart already exist"})
+        }
+        const cart =await new cartSchema({
+            userId:req.body.userId,
+        })
+        try{
+            const temp = await cart.save()
+            res.json(temp)
+        } catch(e){ console.log(e);}
+    }
     
     
 }
-module.exports= new ProductController;
+module.exports= new CartController;
