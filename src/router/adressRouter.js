39b051a70b27b921baf6ef3d18be198c66a63c43
phamquangviet891 <20522153@gm.uit.edu.vn>
2023-10-29 22:17:06 +0700

    updated DEL /address/delete

--- src/router/adressRouter.js
+++ src/router/adressRouter.js
@@ -5,5 +5,5 @@ const addressController = require('../controller/addressController')
 router.get('/getAllAddressByUserId',addressController.getAllAddressByUserId)
 router.post('/addAddress',addressController.addAddress)
 router.patch('/updateAddress',addressController.updateAddress)
-
+router.delete('/delete',addressController.deleteAddress)
 module.exports = router;
