6d274124f47459b73f02eb8dca42fd77d231f076
BaoNguyen <toibaonguyen@gmail.com>
2023-11-20 18:34:13 +0700

    yo wtf man

--- src/model/public/DTO/adminDTO.js
+++ src/model/public/DTO/adminDTO.js
@@ -1,11 +1,8 @@
 export class Admin {
-  constructor(id,email,password,name,birthday,isActive=true)
-  {
-    this.id=id;
-    this.email=email;
-    this.password=password;
-    this.name=name;
-    this.birthday=birthday;
-    this.isActive=isActive
-  }
+  id;
+  email;
+  password;
+  name;
+  birthday;
+  isActive;
 }
