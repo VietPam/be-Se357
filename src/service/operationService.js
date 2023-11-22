f60df0fe26a0baf00f65afa13103c53cc5736a82
BaoNguyen <toibaonguyen@gmail.com>
2023-11-22 22:06:55 +0700

    asfasf

--- src/service/operationService.js
+++ src/service/operationService.js
@@ -1,35 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 import { BuyerDAO } from "../model/private/DAO/buyerDAO";
-import {SellerDAO} from "../model/private/DAO/sellerDAO";
-import {AdminDAO} from "../model/private/DAO/adminDAO";
+import { SellerDAO } from "../model/private/DAO/sellerDAO";
+import { AdminDAO } from "../model/private/DAO/adminDAO";
 //public
-async function getCredential(email,password) {
-  try
-  {
-    const DBconnection=new PrismaClient();
-    DBconnection.
-    const user=await DBconnection.$transaction(async(transaction)=>{
-transaction.buyer.findUnique({
-  where:
-})
-    })
-  }
-  catch(e)
-  {
+async function getCredential(email, password) {
+  try {
+    const DBconnection = new PrismaClient();
+    const [user] = await DBconnection.$transaction([DBconnection.$queryRaw`SELECT "id","email", "password" from  ADMIN Union SELECT id,email, password from  BUYER Union SELECT id,email, password from  SELLER `]);
+    if(user)
+    {
+      
+    }
+  } catch (e) {
     throw e;
   }
-
 }
 function removeCredentialByUserID(userID) {}
-function generateNewAccessToken(userID){}
+function generateNewAccessToken(userID) {}
 
 //private
-function storeAccessTokenToCache(userID,accessToken){}
-function storeRefreshTokenToCache(userID,refreshToken){}
+function storeAccessTokenToCache(userID, accessToken) {}
+function storeRefreshTokenToCache(userID, refreshToken) {}
 
 module.exports = {
   getCredential,
   removeCredentialByUserID,
-  generateNewAccessToken
+  generateNewAccessToken,
 };
-
