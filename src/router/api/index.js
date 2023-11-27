51e58ce17f6ad86a19cc58cbf20aeca3354982a1
BaoNguyen <toibaonguyen@gmail.com>
2023-11-27 15:53:18 +0700

    chưa về nhưng vẫn múa

--- src/router/api/index.js
+++ src/router/api/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 const route = express.Router();
 
-import authRouter from "./auth.js"
+import authRouter from "./auth.js";
+import buyerRouter from "./buyer.js";
 
-route.use("/auth",authRouter);
+route.use("/auth", authRouter);
+route.use("/buyer", buyerRouter);
+
 export default route;
